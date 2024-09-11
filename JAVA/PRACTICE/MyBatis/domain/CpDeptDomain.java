8597d0870ee862107dc7ee861642e7421910cd1f
9y3on <168972177+9y3on@users.noreply.github.com>
2024-09-11 22:03:30 +0900

    practice. MyBatis - Domain

---
+++ JAVA/PRACTICE/MyBatis/domain/CpDeptDomain.java
@@ -0,0 +1,14 @@
+package kr.co.sist.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CpDeptDomain {
+	private String empName;
+	private int deptno;
+
+}
