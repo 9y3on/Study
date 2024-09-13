17c0d920d2934b539721efb87d9567405d7d116d
9y3on <168972177+9y3on@users.noreply.github.com>
2024-09-13 20:23:05 +0900

    practice. MyBatis - Domain

---
+++ JAVA/PRACTICE/MyBatis/domain/JoinDomain.java
@@ -0,0 +1,13 @@
+package kr.co.sist.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinDomain {
+	
+	private int deptno, empno, sal;
+	private String dname, ename;
+
+}
