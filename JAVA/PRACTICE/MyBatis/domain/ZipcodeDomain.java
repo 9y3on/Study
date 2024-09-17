f51de07566474715f0027b19ccaa18315fd8e187
9y3on <168972177+9y3on@users.noreply.github.com>
2024-09-17 21:24:45 +0900

    practice. MyBatis - Domain

---
+++ JAVA/PRACTICE/MyBatis/domain/ZipcodeDomain.java
@@ -0,0 +1,13 @@
+package kr.co.sist.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ZipcodeDomain {
+	private int seq;
+	private String zipcode, sido, gugun, dong, bunji;
+}
