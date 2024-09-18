a3634f2a09da09b2cebeecb5434e8fca51bb178e
9y3on <168972177+9y3on@users.noreply.github.com>
2024-09-18 13:00:57 +0900

    practice. MyBatis - VO

---
+++ JAVA/PRACTICE/MyBatis/vo/BoardVO.java
@@ -0,0 +1,12 @@
+package kr.co.sist.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardVO {
+	private int startNum, endNum;
+	private String fieldName, keyword;
+
+}
